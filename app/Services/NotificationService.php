a1b6bb450a72ef265fe2b4f00ecc4ae1ed172af5
ducviet1995 <vieth.ho@finhay.com.vn>
2021-12-27 22:19:11 +0700

    get list notification

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -13,7 +13,7 @@ class NotificationService
     public function getAll($params, $user)
     {
         $limit = isset($params['limit']) ? $params['limit'] : Consts::LIMIT_DEFAULT;
-        $notifications = Notification::where('user_id', $user->id)->paginate($limit);
+        $notifications = Notification::where('user_id', $user->id)->with('golf', 'event')->paginate($limit);
         return new NotificationCollection($notifications);
     }
 }

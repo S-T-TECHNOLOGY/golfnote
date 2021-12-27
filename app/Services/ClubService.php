b68c96c2bb33393c4d5588fe736166dc13c1b254
ducviet1995 <vieth.ho@finhay.com.vn>
2021-12-28 00:39:25 +0700

    change get club

--- app/Services/ClubService.php
+++ app/Services/ClubService.php
@@ -13,7 +13,13 @@ class ClubService
     public function getAll($params)
     {
         $limit = isset($params['limit']) ? $params['limit'] : Consts::LIMIT_DEFAULT;
-        $clubs = UserClub::paginate($limit);
+        $key = isset($params['key']) ? $params['key'] : Consts::LIMIT_DEFAULT;
+        $clubs = UserClub::when(!empty($key), function ($query) use ($key){
+            return $query->where(function ($query) use ($key) {
+                return $query->where('name', 'like', '%'.$key.'%')
+                    ->orWhere('introduction', 'like', '%'.$key.'%');
+            });
+        })->paginate($limit);
         return new UserClubCollection($clubs);
     }
 }

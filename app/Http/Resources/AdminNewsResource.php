452de213f74cd4550a6a98977c49640a67d586b8
ducviet1995 <vieth.ho@finhay.com.vn>
2022-06-26 17:42:50 +0700

    add start date end date news

--- app/Http/Resources/AdminNewsResource.php
+++ app/Http/Resources/AdminNewsResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class AdminNewsResource extends JsonResource
@@ -14,12 +15,16 @@ class AdminNewsResource extends JsonResource
      */
     public function toArray($request)
     {
+        $dateStart = Carbon::parse($this->start_date);
+        $dateEnd = Carbon::parse($this->end_date);
         return [
             'id' => $this->id,
             'title' => $this->title,
             'description' => $this->description,
             'image' => $this->image,
             'content' => $this->content,
+            'start_date' => $dateStart->format('Y-m-d'),
+            'end_date' => $dateEnd->format('Y-m-d'),
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
         ];
     }
